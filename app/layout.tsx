d7a9f1150b1ddc4e0e3b12d8c25309ce183d7ccc
zlenner <119705166+zlenner@users.noreply.github.com>
2024-06-19 03:59:55 +0300

    absolutely hun

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "tweeto.lol",
-  description: "Remotion and Next.js",
+  description: "tweeto.lol is a simple and easy tool to download X/Twitter videos without losing context (the tweet's text and frame is included in the video).",
   viewport: "width=device-width, initial-scale=1, maximum-scale=1",
 };
 
